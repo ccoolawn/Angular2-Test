3db81c0345ba2187d21d0e76d673e7db07d38abb
Cornell Coulon <ccoulon@cornellcoulon.com>
2018-08-02 09:56:11 -0500

    added artist details

--- src/app/artist-details/artist-details.component.ts
+++ src/app/artist-details/artist-details.component.ts
@@ -3,7 +3,9 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-artist-details',
   templateUrl: './artist-details.component.html',
-  styleUrls: ['./artist-details.component.css']
+  styleUrls: ['./artist-details.component.css'],
+  // tslint:disable-next-line:use-input-property-decorator
+  inputs: ['artist']
 })
 export class ArtistDetailsComponent implements OnInit {
 
