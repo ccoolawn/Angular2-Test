9228fad0023466afff9fc789396fce2a7220ec2f
Cornell Coulon <ccoulon@cornellcoulon.com>
2018-08-01 22:03:58 -0500

    Files for Angular 2 app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import {HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'learnangular';
+export class AppComponent implements OnInit{
+  query: string;
+  artists: object;
+
+  showArtist(item){
+  	this.query = item.name;
+  	item.highlight = !item.highlight;
+  }
+
+  constructor( private http: HttpClient){
+  	this.query = '';
+  }
+
+  ngOnInit(): void {
+  	this.http.get<Object>('../assets/data.json').subscribe(data => { this.artists = data; })
+  }
 }
+
